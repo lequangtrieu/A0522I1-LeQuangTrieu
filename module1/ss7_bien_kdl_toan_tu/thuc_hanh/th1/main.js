ff5a64358af4f5ea3c9fb661a070f3f4f7dae304
unknown <you@example.com>
2022-06-20 18:44:03 +0700

    nop bai

---
+++ module1/ss7_bien_kdl_toan_tu/thuc_hanh/th1/main.js
@@ -0,0 +1,9 @@
+
+let width;
+let height;
+
+width = parseInt ( prompt ('Enter the width') );
+height = parseInt ( prompt ('Enter the height') );
+
+let area = width * height;
+console.log ('the area is ' + area);
