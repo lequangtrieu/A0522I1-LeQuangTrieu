61ca15d08a9fdc01e95e528f1fe4eb7db5f9c71d
unknown <you@example.com>
2022-09-23 19:26:23 +0700

    add

--- module1/ss20_codefpt/lab_2.c
+++ module1/ss20_codefpt/lab_2.c
@@ -1,22 +1,33 @@
-#include<stdio.h>
-
-int main() {
+#include <stdio.h>
+int main(){
 	int a, b;
-	char c;
-	scanf("%d %c %d", &a, &c, &b);
-	switch (c) {
-	case '+':
-		printf("%d", a + b);
-		break;
-	case '-':
-		printf("%d", a - b);
-		break;
-	case '*':
-		printf("%d", a * b);
-		break;
-	case '/':
-		printf("%d", a / b);
-		break;
+    char opera;
+    printf("Nhap phep toan: ");
+    scanf("%c", &opera);
+    printf("Nhap vao 2 so a, b: ");
+    scanf("%d%d", &a, &b);
+	switch(opera){
+		case '+':	
+			printf("\n%d + %d = %d", a, b, a+b);
+			break;
+		case '-':	
+			printf("\n%d - %d = %d", a, b, a-b);
+			break;
+		case '*':
+			printf("\n%d * %d = %d", a, b, a*b);
+			break;
+		case '/':	
+			if(b==0)
+				printf("\nKhong the chia cho 0");
+			else
+				printf("\n%d / %d = %.2f", a, b, (float)a/b);
+			break;	
+		case '%': 
+			printf("\n%d mod %d = %d", a, b, a%b);
+			break;	
+		default:
+			printf("\nKhong co phep toan %c!",opera);
+			break;											
 	}
 	return 0;
 }
